9c7c54d38cc4e66f4b7d5f9d8e56d0ec4041a5ae
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-15 18:18:04 -0400

    finished the part where the app functions but without any html

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
- before_action :find_restaurant, only: %I[show edit update]
+  before_action :find_restaurant, only: %I[show edit update destroy]
 
   def index
     @restaurants = Restaurant.all
@@ -14,24 +14,39 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-   #@restaurant =
+    @restaurant = Restaurant.new(params_restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant), notice: 'Restaurant was
+      successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
-
+    # empty
   end
 
   def update
-
+    if @restaurant.update(params_restaurant)
+      redirect_to @restaurant, notice: 'restaurant successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @restaurant.destroy
+    redirect_to restaurants_path, notice: 'Restaurant was successfully destroyed.'
   end
 
-private
+  private
 
-def find_restaurant
-  @restaurant = Restaurant.find(params[:id])
-end
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def params_restaurant
+    params.require('restaurant').permit(:name, :address, :phone_number, :category)
+  end
 end
